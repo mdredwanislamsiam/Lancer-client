75cce2a5b2435b84bdb71a24ece38c714ec9c88a
Md Redwan Islam <redwanislamsiam13@gmail.com>
2026-02-19 22:43:55 +0600

    final Commit-10

--- src/components/categories/CategoryCard.jsx
+++ src/components/categories/CategoryCard.jsx
@@ -1,9 +1,11 @@
 import { Link } from 'react-router';
 import useCategoriesContext from '../../hooks/useCategoriesContext';
+import useAuthContext from '../../hooks/useAuthContext';
 
 const CategoryCard = ({ category, onDelete }) => {
     
-    const { deleteCategory } = useCategoriesContext();
+	const { deleteCategory } = useCategoriesContext();
+	const { user } = useAuthContext(); 
     // console.log(category);
 	const handleDelete = async () => {
 		if (window.confirm("Are you sure you want to delete this Category?")) {
@@ -20,14 +22,16 @@ const CategoryCard = ({ category, onDelete }) => {
 				<p className="text-base-300 text-xs lg:text-sm">{category.description}</p>
 			</div>
 			<div className="mt-3 flex justify-between gap-2">
-				<Link to={`/dashboard/categories/update/${category.id}`}>
-					<button className="px-3 py-2 font-bold text-white rounded-full bg-gradient-to-r from-[#32435a] to-[#678b9d] hover:from-[#072e4c] hover:to-[#4a7ca3] shadow-lg shadow-[#32435a]/40 transform transition-transform duration-300 hover:scale-105 ring-1 ring-[#678b9d]/50 hover:ring-[#072e4c]/70 text-xs lg:text-md">
+				<Link to={user?.is_staff && `/dashboard/categories/update/${category.id}`}>
+					<button
+						className={`px-3 py-2 font-bold text-white rounded-full bg-gradient-to-r from-[#32435a] to-[#678b9d] hover:from-[#072e4c] hover:to-[#4a7ca3] shadow-lg shadow-[#32435a]/40 transform transition-transform duration-300 hover:scale-105 ring-1 ring-[#678b9d]/50 hover:ring-[#072e4c]/70 text-xs lg:text-md ${!user?.is_staff && "hidden"}`}>
 						Edit Category
 					</button>
 				</Link>
 				<button
+					disabled={!user?.is_staff}
 					onClick={() => handleDelete(category.id)}
-					className="px-3 py-2 font-bold text-white rounded-full bg-gradient-to-r from-[#32435a] to-[#678b9d] hover:from-[#072e4c] hover:to-[#4a7ca3] shadow-lg shadow-[#32435a]/40 transform transition-transform duration-300 hover:scale-105 ring-1 ring-[#678b9d]/50 hover:ring-[#072e4c]/70 text-xs lg:text-md">
+					className={`px-3 py-2 font-bold text-white rounded-full bg-gradient-to-r from-[#32435a] to-[#678b9d] hover:from-[#072e4c] hover:to-[#4a7ca3] shadow-lg shadow-[#32435a]/40 transform transition-transform duration-300 hover:scale-105 ring-1 ring-[#678b9d]/50 hover:ring-[#072e4c]/70 text-xs lg:text-md ${!user?.is_staff && "hidden"}`}>
 					Delete category
 				</button>
 			</div>
